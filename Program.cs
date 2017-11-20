ec517f07e5e533e82096b38ac2239f4140d4e96c
toby2o12 <toby2o12@hotmail.com>
2017-11-20 23:27:05 +0800

    添加nancy

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+namespace dotnetcorenancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var host = new WebHostBuilder()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseKestrel()
+				.UseUrls("http://*:5000")
+				.UseStartup<Startup>()
+				.Build();
+
+			host.Run();
+		}
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Nancy.Owin;
+
+namespace dotnetcorenancy
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseOwin(x => x.UseNancy());
+		}
+	}
+}
